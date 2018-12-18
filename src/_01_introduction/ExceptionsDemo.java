8bcd8fa99ee40821f26a2aadbf141bd64024122b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-17 17:48:45 -0800

    Stated with ExceptionsDemo

--- src/_01_introduction/ExceptionsDemo.java
+++ src/_01_introduction/ExceptionsDemo.java
@@ -15,12 +15,26 @@ public class ExceptionsDemo {
 		//    call to testMethod1 with a try/catch block.
 		//    In the catch block, put a print statement so you know
 		//    if the code is being ran.
+		try {
+			testMethod1(4);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("TestMethod1 was run, exception occured");
+		}
+		
 		
 		// 2. Now call testMethod1 with a value greater than 5. 
 		//    Running your program should not print the statement
 		//    in the catch block.
 		//    You can use the Exception class's printStackTrace() method
 		//    to print a list of methods that are currently in scope.
+		
+		try {
+			testMethod1(6);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block		
+		System.out.println("TestMethod1 ran into an exception");	
+		}
 		
 		// 3. Create a new class called CustomException that extends Exception.
 		//    Add a void method called terminate that simply calls System.exit(0);
@@ -31,7 +45,6 @@ public class ExceptionsDemo {
 		//    This demonstrates how custom exception classes can be used to
 		//    handle exceptions in unlimited ways.
 	}
-	
 	public static void testMethod1(int x) throws Exception {
 		if(x < 5) {
 			throw new Exception();
